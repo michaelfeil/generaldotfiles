c99e9cf33ea5a3822f99989e689a57ac6fa437b3
Michael Feil <63565275+michaelfeil@users.noreply.github.com>
2025-12-10 15:13:45 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ export NONINTERACTIVE=1
 apt-get update
 apt-get install nano apache2-utils build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
               libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libpq-dev \
-              �protobuf-compiler libprotobuf-dev python3-dev curl file git procps musl-tools
+              protobuf-compiler libprotobuf-dev python3-dev curl file git procps musl-tools
 
 # uv and cli tools
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -11,8 +11,14 @@ curl -sSL https://install.python-poetry.org | python3 -
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 rustup target add x86_64-unknown-linux-musl 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew install derailed/k9s/k9s
 
+git config --global user.name michaelfeil
+git config --global user.email 63565275+michaelfeil@users.noreply.github.com
 # pip 
 pip install ruff uv --break-system-packages
 
