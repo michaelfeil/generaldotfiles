5ab03dd8a790843a67a91847bb5438ecec5a3df7
Michael Feil <63565275+michaelfeil@users.noreply.github.com>
2026-01-21 12:40:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-âexport NONINTERACTIVE=1 
+export NONINTERACTIVE=1 
 
 apt-get update
 apt-get install -y nano apache2-utils build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm \
               libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libpq-dev \
-              protobuf-compiler libprotobuf-dev python3-dev curl file git procps musl-tools
+              protobuf-compiler libprotobuf-dev python3-dev curl file git procps musl-tools bash-completion
 
 # uv and cli tools
 curl -LsSf https://astral.sh/uv/install.sh | sh
